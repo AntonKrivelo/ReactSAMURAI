5b3284aa3c4f1dc3f6e5d96affe172810ed99f64
Anton K. <krivelo2017@mail.ru>
2024-05-19 19:22:28 +0300

    37 store,state

--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,106 +1,87 @@
-let rerenderEntireTree = () => {
+let store = {
+  
+_state: {
+  profilePage: {
+      postsData: [
+          {message:"hello my friends, my name is Anton", counter: 18, id:1},
+          {message:"hi, how are you?", counter: 20, id:2},
+          ],
+          newPostText: 'Postt',
+  },
+  messagesPage: {
+      messagesData: [
+          {message: "Hi!", id: 1},
+          {message: "How are you?", id: 2},
+          {message: "Thank you", id: 3},
+          {message: "Hello", id: 4},
+          {message: "My friend", id: 5},
+        ],
+        dialogsData: [
+          {name: "Anton", id: 1},
+          {name: "Nikolays", id: 2},
+          {name: "Viktoria", id: 3},
+          {name: "Sveta", id: 4},
+          {name: "Andrey", id: 5},
+          {name: "Sergey", id: 6},
+        ],
+        newMessageText: 'message',
+  },
+  sideBar: {
+    friendsData: [
+      {name: "Antonio", id: 1},
+      {name: "Viktor", id: 2},
+      {name: "Valera", id: 3},
+    ],
+  },
+  newsPage: {
+    newsPageText: 'Page news!',
+  },
+},
+getState() {
+  return this._state;
+},
+_callSubscriber() {
   console.log('checked!');
-}
+},
+addPost() {
 
-let state = {
-
-
-
-    profilePage: {
-        postsData: [
-            {message:"hello my friends, my name is Anton", counter: 18, id:1},
-            {message:"hi, how are you?", counter: 20, id:2},
-            ],
-            newPostText: 'Postt',
-    },
-    messagesPage: {
-        messagesData: [
-            {message: "Hi!", id: 1},
-            {message: "How are you?", id: 2},
-            {message: "Thank you", id: 3},
-            {message: "Hello", id: 4},
-            {message: "My friend", id: 5},
-          ],
-          dialogsData: [
-            {name: "Anton", id: 1},
-            {name: "Nikolays", id: 2},
-            {name: "Viktoria", id: 3},
-            {name: "Sveta", id: 4},
-            {name: "Andrey", id: 5},
-            {name: "Sergey", id: 6},
-          ],
-          newMessageText: 'message',
-    },
-    sideBar: {
-      friendsData: [
-        {name: "Antonio", id: 1},
-        {name: "Viktor", id: 2},
-        {name: "Valera", id: 3},
-      ],
-    },
-    newsPage: {
-      newsPageText: 'Page news!',
-    },
+  let newPost = {
+    id: 3,
+    message: this._state.profilePage.newPostText,
+    counter: 0,
 
   };
   
-  
-window.state = state;
+  this._state.profilePage.postsData.push(newPost);
+  this._state.profilePage.newPostText = '';
+  this._callSubscriber(this._state);
 
-// POSTS ADD
-
- export let addPost = () => {
-
-    let newPost = {
-      id: 3,
-      message: state.profilePage.newPostText,
-      counter: 0,
-
-    };
-    
-    state.profilePage.postsData.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-
-  };
-
-
-  export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-  }
-
-// POSTS ADD
-
-
-// MESSAGE ADD 
-
-export let addMessage = () => {
+},
+updateNewPostText (newText) {
+  this._state.profilePage.newPostText = newText;
+  this._callSubscriber(this._state);
+},
+addMessage() {
   let newMessage = {
     id: 8,
-    message: state.messagesPage.newMessageText,
+    message: this._state.messagesPage.newMessageText,
   };
 
-  state.messagesPage.messagesData.push(newMessage);
-  state.messagesPage.newMessageText = '';
-  rerenderEntireTree(state);
+  this._state.messagesPage.messagesData.push(newMessage);
+  this._state.messagesPage.newMessageText = '';
+  this._callSubscriber(this._state);
 
-}
+},
+updateNewMessageText (newText) {
+  this._state.messagesPage.newMessageText = newText;
+  this._callSubscriber(this._state);
 
-export let updateNewMessageText = (newText) => {
-
-  state.messagesPage.newMessageText = newText;
-  rerenderEntireTree(state);
-
-}
-
-// MESSAGE ADD
-
+},
+subscribe(observer) {
+  this._callSubscriber = observer;
+},
 
 
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
+};
 
-
-export default state;  
+export default store;  
--- src/index.js
+++ src/index.js
@@ -2,27 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import state from './components/redux/state';
+import store from './components/redux/state';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import { addPost, updateNewPostText, addMessage, updateNewMessageText, subscribe } from './components/redux/state';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
   
-export let rerenderEntireTree = (state) => {
+let rerenderEntireTree = (state) => {
 
   root.render(
 
     <React.StrictMode>
       <BrowserRouter >
-        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} addMessage={addMessage} updateNewMessageText={updateNewMessageText} />
+        <App state={store.getState()} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} addMessage={store.addMessage.bind(store)} updateNewMessageText={store.updateNewMessageText.bind(store)} />
       </BrowserRouter>
     </React.StrictMode>
 
   );
 }
 
+rerenderEntireTree(store.getState());
 
-rerenderEntireTree(state);
-
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
