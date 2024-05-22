f0c83db948daab1bbea9940a4378a06667812fe8
Anton K. <krivelo2017@mail.ru>
2024-05-22 21:04:36 +0300

    delete 12345

--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -24,7 +24,7 @@ _state: {
           {name: "Andrey", id: 5},
           {name: "Sergey", id: 6},
         ],
-        newMessageText: 'message23',
+        newMessageText: 'message',
   },
   sideBar: {
     friendsData: [
