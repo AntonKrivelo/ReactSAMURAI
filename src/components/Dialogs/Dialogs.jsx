0412d6f39ea44144e944ab08b0900ef07c4266b2
Anton K. <krivelo2017@mail.ru>
2024-05-25 20:47:08 +0300

    ADD REDUX, REDUCERS PACK + REDUCER

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Dialogs.module.css";
 import DialogItem from "./DialogItem";
 import Message from "./Message";
-import { submitMessageActionCreator, onMessageChangeActionCreator } from "../redux/state";
+import { submitMessageActionCreator, onMessageChangeActionCreator } from "../redux/store";
 
 const Dialogs = (props) => {
 
--- src/components/redux/profile-reducer.js
+++ src/components/redux/profile-reducer.js
@@ -1,7 +1,18 @@
 let ADD_POST = 'ADD-POST';
 let UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReducer = (state, action) => {
+
+let initialState = {
+    postsData: [
+        {message:"hello my friends, my name is Anton", counter: 18, id:1},
+        {message:"hi, how are you?", counter: 20, id:2},
+        {message:"hi, how are you?", counter: 20, id:3},
+        ],
+    newPostText: '',
+}
+
+const profileReducer = (state = initialState, action) => {
+    debugger;
     if(action.type === ADD_POST) {
         let newPost = {
         id: 6,
